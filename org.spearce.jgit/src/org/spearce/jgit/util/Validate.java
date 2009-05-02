fceae7130850033b3528d1e72b2bcce3b37afc14
Mark Struberg <struberg@yahoo.de>
2009-05-02 15:06:21 +0200

    adding a Validate helper class

---
+++ org.spearce.jgit/src/org/spearce/jgit/util/Validate.java
@@ -0,0 +1,247 @@
+/*
+ * Copyright (C) 2009, Mark Struberg <struberg@yahoo.de>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Git Development Community nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.spearce.jgit.util;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Validate contains a few static helper functions which are very simpilar
+ * to JUnits Assert. They basically make validating function arguments to
+ * be a 1-line call.
+ * 
+ * All validate functions will throw an {@code IllegalArgumentException}
+ * if the validation fails.
+ * 
+ * All validate functions exist twice. 
+ * 
+ * <p>1st function with only the validation option</p>
+ * 
+ * <p>For each validation function a similar 2nd validation function exists
+ * with a list of additional message parameters as Objects in elipsis form. 
+ * This is used instead of simply passing a message String due to performance reasons!
+ * When using a message string, all parameters would have to be string concatenated
+ * before the call, even if no problem arises which would cost performance.</br>
+ * Instead of this, we will concatenate (with spaces) all given msgObjects.toString() 
+ * only in case of a failed validation!</p>
+ * 
+ * <h3>Examples:</h3>
+ * <p>
+ * Simply validating an Argument without further message:
+ * <pre>
+ * public void myFn(String argString, Integer argInt) {
+ *     Validate.notNull(argString);
+ *     Validate.notNull(argInt);
+ *     Validate.isTrue(argInt.intValue > 3);
+ * }
+ * </pre>
+ * <p>
+ * 
+ * <p>
+ * Validating an Argument and adding a message to the IllegalArgumentException:
+ * <pre>
+ * public void myFn(String argString, Integer argInt) {
+ *     Validate.notNull(argInt, "Integer parameter must be set);
+ *     Validate.isTrue(argInt.intValue > 3, "Integer parameter must be <=3 but actually is", argInt);
+ * }
+ * </pre>
+ * <p>
+ */
+public class Validate {
+
+	/**
+	 * Private ct since this class should not be instantiated!
+	 */
+	private Validate() {
+		super();
+	}
+	
+	/**
+	 * Validate that o is not <code>null</code>
+	 * 
+	 * @param o Object to validate
+	 */
+	public static void notNull(Object o) {
+		if (o == null) {
+			throw new IllegalArgumentException("The validated object is null!");
+		}
+	}
+
+	/**
+	 * Validate that o is not <code>null</code>
+	 * 
+	 * @param o Object to validate
+	 * @param msgObjects additional Objects added as text message to the InvalidArgumentException
+	 */
+	public static void notNull(Object o, Object... msgObjects) {
+		if (o == null) {
+			throw new IllegalArgumentException(getMessage(msgObjects));
+		}
+	}	
+
+	/**
+	 * Validate that b is <code>true</code>
+	 * 
+	 * @param b boolean to validate
+	 */
+	public static void isTrue(boolean b) {
+		if (!b) {
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	/**
+	 * Validate that b is <code>true</code>
+	 * 
+	 * @param b boolean to validate
+	 * @param msgObjects additional Objects added as text message to the InvalidArgumentException
+	 */
+	public static void isTrue(boolean b, Object... msgObjects) {
+		if (!b) {
+			throw new IllegalArgumentException(getMessage(msgObjects));
+		}
+	}
+
+	/**
+	 * Validate that the given String is not <code>null</code> and is not empty.
+	 * 
+	 * @param string the array to validate
+	 */
+	public static void notEmpty(String string) {
+		if (string == null || string.length() == 0) {
+			throw new IllegalArgumentException("The validated String is empty!");
+		}
+	}
+	
+	/**
+	 * Validate that the given String is not <code>null</code> and is not empty.
+	 * 
+	 * @param string the array to validate
+	 * @param msgObjects additional Objects added as text message to the InvalidArgumentException
+	 */
+	public static void notEmpty(String string, Object... msgObjects) {
+		if (string == null || string.length() == 0) {
+			throw new IllegalArgumentException(getMessage(msgObjects));
+		}
+	}
+
+	/**
+	 * Validate that arr is not <code>null</code> and has at least 1 entry.
+	 * 
+	 * @param arr the array to validate
+	 */
+	public static void notEmpty(Object[] arr) {
+		if (arr == null || arr.length == 0) {
+			throw new IllegalArgumentException("The validated Object arrray is empty!");
+		}
+	}
+	
+	/**
+	 * Validate that arr is not <code>null</code> and is not empty.
+	 * 
+	 * @param arr the array to validate
+	 * @param msgObjects additional Objects added as text message to the InvalidArgumentException
+	 */
+	public static void notEmpty(Object[] arr, Object... msgObjects) {
+		if (arr == null || arr.length == 0) {
+			throw new IllegalArgumentException(getMessage(msgObjects));
+		}
+	}
+	
+	/**
+	 * Validate that the given Collection is not <code>null</code> and is not empty.
+	 * 
+	 * @param coll the Collection to validate
+	 */
+	public static void notEmpty(Collection coll) {
+		if (coll == null || coll.isEmpty()) {
+			throw new IllegalArgumentException("The validated Collection is empty!");
+		}
+	}
+	
+	/**
+	 * Validate that the given Collection is not <code>null</code> and is not empty.
+	 * 
+	 * @param coll the Collection to validate
+	 * @param msgObjects additional Objects added as text message to the InvalidArgumentException
+	 */
+	public static void notEmpty(Collection coll, Object... msgObjects) {
+		if (coll == null || coll.isEmpty()) {
+			throw new IllegalArgumentException(getMessage(msgObjects));
+		}
+	}
+	
+	/**
+	 * Validate that the given Map is not <code>null</code> and is not empty.
+	 * 
+	 * @param map the array to validate
+	 */
+	public static void notEmpty(Map map) {
+		if (map == null || map.isEmpty()) {
+			throw new IllegalArgumentException("The validated Map is empty!");
+		}
+	}
+	
+	/**
+	 * Validate that the given Map is not <code>null</code> and is not empty.
+	 * 
+	 * @param map the Map to validate
+	 * @param msgObjects additional Objects added as text message to the InvalidArgumentException
+	 */
+	public static void notEmpty(Map map, Object... msgObjects) {
+		if (map == null || map.isEmpty()) {
+			throw new IllegalArgumentException(getMessage(msgObjects));
+		}
+	}
+	
+	
+	/**
+	 * private helper function to create an error message from the given Objects
+	 * @param msgObjects
+	 * @return concatenated String representation of all the objects
+	 */
+	private static String getMessage(Object... msgObjects) {
+		StringBuffer sb = new StringBuffer("Validation failed: [");
+		for(Object msgPart : msgObjects) {
+			sb.append(' ').append(msgPart);
+		}
+		sb.append(']');
+		return sb.toString();
+	}
+
+}
